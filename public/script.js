3db701e5a156cfef72c3b74092ee9be4e45c7556
2ndyrGroupC <2ndyrGroupC@pn.local>
2019-09-26 10:34:04 +0800

    Back again

--- public/script.js
+++ public/script.js
@@ -1,12 +1,12 @@
 var uname = "";
 var id = "";
+var typer = "";
 var socket = io();
 var typing = 0;
 var typingpm = 0;
 var stilltying = false;
-var listOfUsers = [];
-var typer = "";
 var firsttyper = true;
+var listOfUsers = [];
 var conversations = [];
 var typingtext = " is now typing 💬";
 var mesId = 0;
@@ -16,12 +16,10 @@ var mesId = 0;
 $(document).ready(function () {
 
     var socket = io();
-    $('#inputName').submit(function () {
+    $('#name').submit(function () {
         for (var i = 0; i < 10; ++i) {
             id += (Math.floor((Math.random() * 9) + 0)).toString();
         }
-
-
         socket.on('user login', function (msg) {
             if (!listOfUsers.includes(msg.name + ": " + msg.id) && msg.id != id) {
                 listOfUsers.push(msg.name + ": " + msg.id);
@@ -29,8 +27,18 @@ $(document).ready(function () {
                 socket.emit('user login', { id: id, name: uname });
             }
         });
-
-
+        socket.on('chat message', function (msg) {
+            $("#msgContainer").scrollTop($("#msgContainer")[0].scrollHeight);
+            if (msg.id == id && msg.name == uname) {
+                publicChat('myMess', "<b>Me</b>: " + msg.message);
+            } else {
+                typer = "";
+                firsttyper = true;
+                typingtext = " is typing 💬";
+                $("#typing").remove();
+                publicChat('othersMess', "<b>" + msg.name + "</b>: " + msg.message);
+            }
+        });
         socket.on('private message', function (msg) {
             if (msg.receipient == uname + id) {
                 if (!conversations.includes(msg.name + msg.id)) {
@@ -46,14 +54,12 @@ $(document).ready(function () {
             }
         });
 
-
-
         socket.on('typing', function (msg) {
             if (msg.type == "private") {
                 if (msg.receipient == uname + id) {
                     $("#" + msg.name + msg.id).find("#typingpm").remove();
                     clearInterval(typingpm);
-                    $("#" + msg.name + msg.id).find("#pm").append("<div class='pmdivh' id='typingpm'><div class='typingPM'><p>" + msg.name + " is typing..." + "</p></div></div>");
+                    $("#" + msg.name + msg.id).find("#pm").append("<div class='pmdivh' id='typingpm'><div class='typingPM'><p>" + msg.name + " is typing 💬" + "</p></div></div>");
                     typingpm = setInterval(function () { $("#" + msg.name + msg.id).find("#typingpm").remove(); }, 1000);
                 }
             } else if (msg.id != id && msg.name != uname) {
@@ -82,26 +88,12 @@ $(document).ready(function () {
             }
         });
 
-
-        socket.on('chat message', function (msg) {
-            $("#messagesHolder").scrollTop($("#messagesHolder")[0].scrollHeight);
-            if (msg.id == id && msg.name == uname) {
-                publicChat('myMess', "<b>Me</b>: " + msg.message);
-            } else {
-                typer = "";
-                firsttyper = true;
-                typingtext = " is typing 💬";
-                $("#typing").remove();
-                publicChat('othersMess', "<b>" + msg.name + "</b>: " + msg.message);
-            }
-        });
-
         uname = $("#username").val().split(" ").join("_");
         if (!uname.includes(": ")) {
             $(".uname").text(uname);
             socket.emit('user login', { id: id, name: uname });
             $("#title").text(uname);
-            $("#inputName").hide();
+            $("#name").hide();
             $(".container").show();
         } else {
             alert("Invalid username!");
@@ -111,9 +103,9 @@ $(document).ready(function () {
 
     $('#chatbox').submit(function (e) {
         e.preventDefault();
-        if ($('#m').val() != "") {
-            socket.emit('chat message', { id: id, name: uname, message: $('#m').val() });
-            $('#m').val('');
+        if ($('#msg').val() != "") {
+            socket.emit('chat message', { id: id, name: uname, message: $('#msg').val() });
+            $('#msg').val('');
         }
     });
 });
@@ -147,8 +139,6 @@ $(document).on("mouseleave", ".newMes", function () {
     $(this).css({ "box-shadow": "none" });
 });
 
-
-
 $(document).on('click', '#close', function () {
     $(this).closest("div").hide();
 });
@@ -171,7 +161,6 @@ function publicChat(divType, message) {
     $('#messages').append("<div id='mesDivHolder'><div class='newMes' id='" + divType + "'><h5 id='me'>" + message + "</h5></div><div>");
 }
 
-
 $(document).on('click', '.otherUser', function () {
     if (!conversations.includes($(this).find("#User").text() + $(this).find("#userId").text())) {
         conversations.push($(this).find("#User").text() + $(this).find("#userId").text());
@@ -185,6 +174,7 @@ function privateChat(converId, name, pmessage) {
     $('#chatboxes').prepend("<div id='" + converId + "' class='newChatWindow'><p id='close'>x</p><h3 id='chatmateName'>" + name + "</h3><div id='pmholder'><p id='pm'></p></div><form id='chatbox2'><input id='m2' autofocus='' autocomplete='off' placeholder='Send a message'/></form></div>");
     $("#" + converId).find("#pm").append("<div class='pmdivh'><div class='pmdiv' id='ntm'><p id='PM'><b>" + name + "</b>: " + pmessage + "</p></div></div>");
 }
+
 $(document).on('submit', "#chatbox2", function (e) {
     e.preventDefault();
     if ($(this).find("#m2").val() != "") {
